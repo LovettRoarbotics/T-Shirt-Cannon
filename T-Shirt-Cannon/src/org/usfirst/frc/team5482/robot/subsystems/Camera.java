ecc64cbf799df4275ea112b1e965d93d60c68251
im <byron.lathi@student.lovett.org>
2016-09-02 11:54:53 -0400

    add Camera subsystem

---
+++ T-Shirt-Cannon/src/org/usfirst/frc/team5482/robot/subsystems/Camera.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team5482.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Camera extends Subsystem {
+    
+    public void reset(){
+    	
+    }
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
