b727eeba577ff02e7047e4924bc249c8436926d0
im <byron.lathi@student.lovett.org>
2016-09-02 18:25:21 -0400

    Update Led subsystem
    added a test function as well as changed how data is sent to the rgb
    strip.

--- T-Shirt-Cannon/src/org/usfirst/frc/team5482/robot/subsystems/Led.java
+++ T-Shirt-Cannon/src/org/usfirst/frc/team5482/robot/subsystems/Led.java
@@ -1,47 +1,75 @@
 package org.usfirst.frc.team5482.robot.subsystems;
 
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc.team5482.robot.HardwareAdapter;
+import org.usfirst.frc.team5482.robot.commands.LedColor;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class Led extends Subsystem {
-	
+
 	public byte rgb[] = new byte[3];
 
-	public void rainBow() {
-
-	}
-
-	public void pulse(byte a, byte b, byte c, byte x, byte y, byte z) {
-		double colorA[] = { a, b, c };
-		double colorB[] = { x, y, z };
-	}
-
-	public void color(byte a, byte b, byte c) {
-		byte colorA[] = { a, b, c };
-
-		for (int i = 0; i < 3; i++) {
-			rgb[i] = colorA[i];
-		}
-
-		HardwareAdapter.kI2C.transaction(rgb, 3, null, 0);
-	}
-	
-	public void setColor(byte a, byte b, byte c){
-		byte color[] = {a, b, c};
+	public void setColor(byte a, byte b, byte c) {
+		byte color[] = { a, b, c };
 		rgb = color;
 		HardwareAdapter.kI2C.transaction(color, 3, null, 0);
+		Timer.delay(0.1);
+	}
+
+	public byte[] getColor() {
+		return rgb;
 	}
 	
-	public byte[] getColor(){
-		return rgb;
+	public void testLed(){
+		byte a, b, c;
+		a = 127;
+		b = 127;
+		c = 127;
+		setColor(a, b, c);
+		a = 0;
+		b = 127;
+		c = 127;
+		setColor(a, b, c);
+		a = 127;
+		b = 0;
+		c = 127;
+		setColor(a, b, c);
+		a = 127;
+		b = 127;
+		c = 0;
+		setColor(a, b, c);
+		a = 127;
+		b = 127;
+		c = 127;
+		setColor(a, b, c);
+		a = 127;
+		b = 0;
+		c = 0;
+		setColor(a, b, c);
+		a = 0;
+		b = 127;
+		c = 0;
+		setColor(a, b, c);
+		a = 0;
+		b = 0;
+		c = 127;
+		setColor(a, b, c);
+		a = 0;
+		b = 0;
+		c = 0;
+		setColor(a, b, c);
+		a = 127;
+		b = 127;
+		c = 127;
+		setColor(a, b, c);
+		
 	}
 
 	public void initDefaultCommand() {
-		// Set the default command for a subsystem here.
-		// setDefaultCommand(new Command());
+		setDefaultCommand(new LedColor());
 	}
 }
