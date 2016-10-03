c9a56e3a89d265bacf71a9a004d98c7d117d2122
im <byron.lathi@student.lovett.org>
2016-10-03 07:50:22 -0400

    Removed unused Folders

--- Basic-T-Shirt-Cannon/src/org/usfirst/frc/team5482/robot/RobotMap.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team5482.robot;
-
-public class RobotMap {
-	
-	public static final int mainStickPort = 0;
-	public static final int fire = 0;
-	public static final int safety = 1;
-	
-}
